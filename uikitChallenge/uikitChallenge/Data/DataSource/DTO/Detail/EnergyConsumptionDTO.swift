5f9655e871df3b15dbf2a093c3e17e12afb750fc
Daniel Belmonte Valero <77160617+DaniBelmonte@users.noreply.github.com>
2024-12-11 13:50:22 +0100

    Add uikit project

---
+++ uikitChallenge/uikitChallenge/Data/DataSource/DTO/Detail/EnergyConsumptionDTO.swift
@@ -0,0 +1,12 @@
+//
+//  EnergyConsumptionDTO.swift
+//  challenge
+//
+//  Created by Daniel Belmonte Valero on 10/12/24.
+//
+
+import Foundation
+
+struct EnergyConsumptionDTO: Codable {
+    let type: String
+}
